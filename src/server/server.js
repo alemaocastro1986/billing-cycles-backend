a8ffea625431dc9079ce65f64a561b58378a1ee3
Andrius Castro <alemaocastro1986@gmail.com>
2018-06-27 09:45:24 -0300

    Add modulo server

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const enviroment = require('../config/enviroments');
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(enviroment.server.port, ()=>{
+   console.log(`BACKEND is running on port ${enviroment.server.port}.`);
+})
+
