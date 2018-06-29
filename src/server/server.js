1b7e03b7511ea78f43f592c77fda3de86437c67b
Andrius Castro <alemaocastro1986@gmail.com>
2018-06-29 10:37:50 -0300

    Add queryParser

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const allowCors  = require('../config/cors')
+const queryParser = require('express-query-int')
 
 const enviroment = require('../config/enviroments');
 const server = express()
@@ -8,6 +9,7 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(enviroment.server.port, ()=>{
    console.log(`BACKEND is running on port ${enviroment.server.port}.`);
