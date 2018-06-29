cee57b2cad3e5bcdc8b9e4eccc17896c893cb46b
Andrius Castro <alemaocastro1986@gmail.com>
2018-06-29 10:30:08 -0300

    Add cors middleware

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const allowCors  = require('../config/cors')
 
 const enviroment = require('../config/enviroments');
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(enviroment.server.port, ()=>{
    console.log(`BACKEND is running on port ${enviroment.server.port}.`);
